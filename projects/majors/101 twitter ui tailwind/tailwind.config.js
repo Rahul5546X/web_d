640e7342e1a1a2884ed11ecbbaff4afbc6d44f2f
Rahul5546X <142916864+Rahul5546X@users.noreply.github.com>
2024-02-21 22:13:03 +0530

    twitter ui clone

---
+++ projects/majors/101 twitter ui tailwind/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  // content: [".*.html"],
+  content: ["*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+
+
+
+
+
